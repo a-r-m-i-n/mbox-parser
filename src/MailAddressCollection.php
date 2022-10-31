10a2825800856f2d039fdf01462f9295ab8db3c1
Armin Vieweg <vieweg@iwkoeln.de>
2022-10-31 16:58:40 +0100

    [TASK] Use doctrine/collection

--- src/MailAddressCollection.php
+++ src/MailAddressCollection.php
@@ -2,57 +2,17 @@
 
 namespace Armin\MboxParser;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 /**
- * @implements \ArrayAccess<int, MailAddress>
+ * @extends  ArrayCollection<int, MailAddress>
  */
-class MailAddressCollection implements \ArrayAccess, \Countable
+class MailAddressCollection extends ArrayCollection
 {
-    /**
-     * @var array|MailAddress[]
-     */
-    private $addresses = [];
-
-    public function __construct(array $addresses = [])
-    {
-        foreach ($addresses as $address) {
-            $this->add($address);
-        }
-    }
-
-    public function add(MailAddress $address): void
-    {
-        $this->addresses[] = $address;
-    }
-
-    public function offsetExists($offset): bool
-    {
-        return isset($this->addresses[$offset]) || array_key_exists($offset, $this->addresses);
-    }
-
-    public function offsetGet($offset): ?MailAddress
-    {
-        return $this->addresses[$offset] ?? null;
-    }
-
-    public function offsetSet($offset, $value): void
-    {
-        $this->addresses[$offset] = $value;
-    }
-
-    public function offsetUnset($offset): void
-    {
-        unset($this->addresses[$offset]);
-    }
-
-    public function count(): int
-    {
-        return count($this->addresses);
-    }
-
-    public function __toString()
+    public function __toString(): string
     {
         $parts = [];
-        foreach ($this->addresses as $address) {
+        foreach ($this->getValues() as $address) {
             $parts[] = (string)$address;
         }
 
